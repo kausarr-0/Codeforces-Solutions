f80b2d82c9ce7695e0871bdd4fc77ded3bbcbf53
Md. Kausar Babu <149301075+kausarr-0@users.noreply.github.com>
2024-12-21 01:01:57 +0600

    Codeforces Round 994 (Div. 2)

---
+++ A_MEX_Destruction.cpp
@@ -0,0 +1,86 @@
+/* In the Name of ALLAH, the most gracious, the most merciful  */
+#include <bits/stdc++.h>
+using namespace std;
+#define nl endl
+#define ll long long
+#define cy cout<<"YES"<<nl
+#define cn cout<<"NO"<<nl
+#define dis(x) cerr<<#x<<" = "<<x<<nl
+#define all(v) (v).begin(),(v).end()
+#define si(v) int((v).size())
+#define pb push_back
+const int inf = 1e9;
+const ll INF = 1e18;
+const int mod = 998244353;
+const int N = 2e5 + 5;
+// Formula X:
+ll findIndex(const vector<ll>& v, ll e) { auto it =find(all(v),e);
+    if(it!=v.end()){return distance(v.begin(),it);}return -1; }
+ll gcd(ll a,ll b) { if(b==0)return a;return gcd(b,a%b); }//__gcd 
+ll lcm(ll a,ll b) { return(1LL*a*b)/__gcd(a, b); }
+ll sumFrom_IToJ(ll i,ll j) { return((j - i + 1LL)*(i + j))/2; }
+bool sorta_2nd(const pair<int,int>&a,const pair<int,int>&b)
+    { return(a.second<b.second); /* Use : sort(all(vec),sorta_2nd);*/ }
+bool isPrime(ll n) { if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;
+    for(ll i=5;i*i<=n;i=i+6) if(n%i==0||n%(i+2)==0)return false;return true; }
+bool isPowerOfTwo(ll n) { return(n>0) && (n&(n-1))==0; }
+
+void solve() {
+    ll a;cin>>a;
+    vector<ll>v(a) ;
+    map<ll,ll>m;
+    for(auto &i:v)
+    {
+        cin>>i;
+        m[i]++;
+    }
+    if(m[0]==a)
+    {
+      //  dis(m[0]);
+        cout<<0<<nl;
+        return;
+    }
+    else if(m[0]==0)
+    {
+        // dis(m[0]);
+        cout<<1<<nl;
+        return;
+    }
+    ll cnt=0,z=0,zz=0;
+    for(ll i=0;i<a;i++)
+    {
+        if(v[i]==0)
+        z++;
+        else break;
+    }
+    if(z==m[0])
+    {
+        cout<<1<<nl;
+        return;
+    }
+
+    for(ll i=a-1;i>=0;i--)
+    {
+        if(v[i]==0)
+        zz++;
+        else break;
+    }
+    if(zz==m[0])
+    cout<<1<<nl;
+    else if(zz+z==m[0])
+    cout<<1<<nl;
+    else cout<<2<<nl;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        //cout << "Case " << i << ": ";
+        solve();
+    }
+ return 0;
+}

d49396c8a53759482d510f9e71d3f45e5e56bc3f
Md. Kausar Babu <149301075+kausarr-0@users.noreply.github.com>
2025-08-30 00:52:51 +0600

    Codeforces Round 1046 (Div. 2)

---
+++ B_Like_the_Bitset.cpp
@@ -0,0 +1,84 @@
+// In the Name of ALLAH, the most gracious, the most merciful
+// Watch the solve video on -> youtube.com/@CodeConnectt 
+// Created : 2025-08-28 21:21:13
+#include <bits/stdc++.h>
+using namespace std;
+#define nl endl
+#define ll long long
+#define cy cout<<"YES"<<nl
+#define cn cout<<"NO"<<nl
+#define dis(x) cerr<<#x<<" = "<<x<<nl
+#define all(v) (v).begin(),(v).end()
+#define si(v) int((v).size())
+#define pb push_back
+const int inf = 1e9;
+const ll INF = 1e18;
+const int mod = 998244353;
+const int Big = 2e5 + 5;
+// Formula X:
+#define lbval(v, val) ((lower_bound(all(v),val)!=(v).end())?*lower_bound(all(v),val):-1)
+#define lbinx(v, val) (lower_bound(all(v), val) - (v).begin())
+ll findIndex(const vector<ll>& v, ll e) { auto it =find(all(v),e);
+    if(it!=v.end()){return distance(v.begin(),it);}return -1; }
+ll lcm(ll a,ll b) { return(1LL*a*b)/__gcd(a, b); }
+ll total_digit(ll i) { ll m = log10(i) + 1; return m; }
+ll sumFrom_IToJ(ll i,ll j) { return((j - i + 1LL)*(i + j))/2; }
+bool sorta_2nd(const pair<int,int>&a,const pair<int,int>&b)
+    { return(a.second<b.second); /* Use : sort(all(vec),sorta_2nd);*/ }
+bool isPrime(ll n) { if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;
+    for(ll i=5;i*i<=n;i=i+6) if(n%i==0||n%(i+2)==0)return false;return true; }
+bool isPowerOfTwo(ll n) { return(n>0) && (n&(n-1))==0; }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {x+=0x9e3779b97f4a7c15;x=(x^(x>>30))*0xbf58476d1ce4e5b9;
+        x=(x^(x>>27))*0x94d049bb133111eb; return x^(x>>31);}
+    size_t operator()(long long x)const{static const uint64_t FIXED_RANDOM=
+        chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x+FIXED_RANDOM);}
+};
+
+void solve() {
+    ll n,k;cin>>n>>k;
+    string s;cin>>s;
+
+    if (s.find(string(k,'1'))!=string::npos) 
+    {
+        cn;
+        return;
+    }
+
+    cy;
+    vector<ll>f(n);
+    
+    ll mn=1,mx=n;
+    for(ll i=0;i<n;i++)
+    {
+        if(s[i]=='0')
+        {
+            f[i]=mx--;
+        }
+    }
+
+    for(ll i=n-1;i>=0;i--)
+    {
+        if(s[i]=='1')
+        {
+            f[i]=mn++;
+        }
+    }
+
+    for(auto &it:f)
+    cout<<it<<' ';
+    cout<<nl;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        //cout << "Case " << tc << ": ";
+        solve();
+    }
+ return 0;
+}

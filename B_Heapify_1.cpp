d7a2ec9c815e44c1dc968d2057f3a481abcc4334
Md. Kausar Babu <149301075+kausarr-0@users.noreply.github.com>
2026-02-16 21:51:25 +0600

    Codeforces Round 1080 (Div. 3)

---
+++ B_Heapify_1.cpp
@@ -0,0 +1,92 @@
+// In the Name of ALLAH, the most gracious, the most merciful
+// Watch the solve video on -> youtube.com/@CodeConnectt 
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define ll long long
+#define vl vector<ll>
+#define cy cout<<"YES"<<nl
+#define cn cout<<"NO"<<nl
+#define all(v) (v).begin(),(v).end()
+#define si(v) int((v).size())
+#define pb push_back
+const int inf = 1e9;
+const ll INF = 1e18;
+const int mod = 998244353;
+const int Big = 2e5 + 5;
+#ifndef ONLINE_JUDGE
+#define dis(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+template <typename T>
+void _dbg(const char* n, T&& v) {cerr<<n<<" = "<<v<<'\n';}
+template <typename T, typename... A>
+void _dbg(const char*n,T&& v,A&&... a){const char*c=strchr(n+1,',');
+    cerr.write(n,c-n)<<" = "<<v<<" | ";_dbg(c+1, a...); }
+#else
+#define dis(...)
+#endif
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {x+=0x9e3779b97f4a7c15;x=(x^(x>>30))*0xbf58476d1ce4e5b9;
+        x=(x^(x>>27))*0x94d049bb133111eb; return x^(x>>31);}
+    size_t operator()(long long x)const{static const uint64_t FIXED_RANDOM=
+        chrono::steady_clock::now().time_since_epoch().count();return splitmix64(x+FIXED_RANDOM);}};
+//..................................................................................................
+
+void solve() {
+    ll n;cin>>n;
+    vl v(n+1),st(n+1);
+    for(ll i=1;i<=n;i++)
+    {
+        cin>>v[i];
+        st[i]=v[i];
+    }
+
+    sort(st.begin()+1,st.end()) ;
+
+    if(is_sorted(v.begin()+1,v.end()))
+    {
+        cy;
+        return;
+    }
+
+    vl vis(n+1,0);
+    for(ll i=1;i<=n;i++)
+    {
+        if(vis[i])
+        continue;
+
+        vl cur,vv;
+
+        ll c=i;
+        while(c<=n)
+        {
+            vis[c]=1;
+            cur.pb(c);
+            vv.pb(v[c]);
+            c*=2;
+        }
+        sort(all(vv));
+
+        for(ll j=0;j<si(cur);j++)
+        {
+            if(vv[j]!=st[cur[j]])
+            {
+                cn;
+                return;
+            }
+        }
+
+    }
+    cy;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        //cout << "Case " << tc << ": ";
+        solve();
+    }
+ return 0;
+}
